41ad29a377ba6b3260728f8f3f241a0db632fc86
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2020-10-09 19:28:42 -0400

    Update: Replaced innerhtml with outerhtml in the replace-title-entities() Add: Made a test for fixing double encoded html entities in titles

--- src/backend/utils/html/replace-title-entities.js
+++ src/backend/utils/html/replace-title-entities.js
@@ -1,11 +1,17 @@
 const entities = require('entities');
 
+function decode(codeElement) {
+  // decode twice for double encoded html entities
+  const result = entities.decodeHTML(codeElement);
+  return entities.decodeHTML(result);
+}
+
 module.exports = function (dom) {
   if (!(dom && dom.window && dom.window.document)) {
     return;
   }
 
   dom.window.document.querySelectorAll('h1.MuiTypography-h1').forEach((h1Tag) => {
-    h1Tag.outerHTML = entities.decodeHTML(h1Tag.outerHTML);
+    h1Tag.outerHTML = decode(h1Tag.outerHTML);
   });
 };
--- test/replace-title-entities.test.js
+++ test/replace-title-entities.test.js
@@ -21,10 +21,14 @@ test('The value returned by decodeTitles() is of type object', () => {
   expect(typeof data).toBe('object');
 });
 
-// test('Ampersand html entities from the title should be decoded', () => {
-//   const x = '';
-//   //
-// });
+test('Titles with double encoding on html entities should be properly singly encoded', () => {
+  const original =
+    '<h1 class="MuiTypography-root jss290 MuiTypography-h1" title="Hello World"> Hello World &amp;amp; Hello World </h1>';
+  const properEncoding =
+    '<h1 class="MuiTypography-root jss290 MuiTypography-h1" title="Hello World"> Hello World &amp; Hello World </h1>';
+  const data = decodeTitles(original);
+  expect(data[0].outerHTML).toBe(properEncoding);
+});
 
 // test('Greater than html entities from the title should be decoded', () => {
 //   const x = '';
