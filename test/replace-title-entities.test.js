345a68f6ba568cea3fadc51cb979cc47e258a4b4
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2020-10-11 02:06:06 -0400

    Added: Tests that confirm the double encoded entities get properly singly encoded

--- test/replace-title-entities.test.js
+++ test/replace-title-entities.test.js
@@ -21,7 +21,7 @@ test('The value returned by decodeTitles() is of type object', () => {
   expect(typeof data).toBe('object');
 });
 
-test('Titles with double encoding on html entities should be properly singly encoded', () => {
+test('Double encoded ampersand html entities should be properly singly encoded', () => {
   const original =
     '<h1 class="MuiTypography-root jss290 MuiTypography-h1" title="Hello World"> Hello World &amp;amp; Hello World </h1>';
   const properEncoding =
@@ -30,17 +30,20 @@ test('Titles with double encoding on html entities should be properly singly enc
   expect(data[0].outerHTML).toBe(properEncoding);
 });
 
-// test('Greater than html entities from the title should be decoded', () => {
-//   const x = '';
-//   //
-// });
+test('Double encoded greater than html entities from the title should be singly encoded', () => {
+  const original =
+    '<h1 class="MuiTypography-root jss290 MuiTypography-h1" title="Hello World"> Hello World &amp;gt; Hello World </h1>';
+  const properEncoding =
+    '<h1 class="MuiTypography-root jss290 MuiTypography-h1" title="Hello World"> Hello World &gt; Hello World </h1>';
+  const data = decodeTitles(original);
+  expect(data[0].outerHTML).toBe(properEncoding);
+});
 
-// test('Less than html entities from the title should be decoded', () => {
-//   const x = '';
-//   //
-// });
-
-// test('Multiple titles should be decoded', () => {
-//   const x = '';
-//   //
-// });
+test('Double encoded less than html entities from the title should be singly encoded', () => {
+  const original =
+    '<h1 class="MuiTypography-root jss290 MuiTypography-h1" title="Hello World"> Hello World &amp;lt; Hello World </h1>';
+  const properEncoding =
+    '<h1 class="MuiTypography-root jss290 MuiTypography-h1" title="Hello World"> Hello World &lt; Hello World </h1>';
+  const data = decodeTitles(original);
+  expect(data[0].outerHTML).toBe(properEncoding);
+});
